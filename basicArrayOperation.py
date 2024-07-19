155e664df9ea7796b8046592b9e7876bdf212684
Ashray04 <156450734+Ashray04@users.noreply.github.com>
2024-07-19 23:39:06 +0530

    Create basicArrayOperation.py

---
+++ basicArrayOperation.py
@@ -0,0 +1,11 @@
+array_list = [2,1,3]
+print("Original:",array_list)
+
+array_list[2]=10
+print("Modified List:",array_list)
+
+array_list.append(5)
+print("List after appending:",array_list)
+
+array_list.remove(1)
+print("List after removing an element:", array_list)
