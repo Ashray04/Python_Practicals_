6f1c22762924f00c0ff1419af0b58bbd32124d18
Ashray04 <156450734+Ashray04@users.noreply.github.com>
2024-08-24 14:17:39 +0530

    Update modulo_function.py

--- PRACTICAL - 04/pattern0.py
+++ PRACTICAL - 04/pattern0.py
@@ -1,23 +1,22 @@
-def modulo (a,b) :
-    if(b == 0) :
-        return "Error Divisor can't be Zero!"
+def simple_modulo(a, b):
+    if b == 0:
+        return "Error: Divisor can't be Zero!"
+    
     first_number = abs(a)
     second_number = abs(b)
-    mod = 0;
-    sum = 0;
-    while (sum<first_number) : 
-        sum += second_number
-    if(sum==first_number) :
-            return 0
-    else :
-            mod = first_number-sum+second_number
-    if(a > 0 and b > 0) :
-        return mod;
-    elif(a>0 and b<0 ) :
-        return mod-second_number;
-    elif(a < 0 and b < 0) :
-        return mod - 2*mod;
-    elif(a<0 and b>0) :
-        return mod-second_number+(2*abs(mod-second_number))
-    else :
-        return 0
+
+    while first_number >= second_number:
+        first_number -= second_number
+    
+    remainder = first_number
+
+    if a < 0:
+        remainder = remainder
+    
+    return remainder
+aa=simple_modulo(-10,3)
+bb=simple_modulo(-10,-3)
+cc=simple_modulo(10,3)
+print(aa)
+print(bb)
+print(cc)
